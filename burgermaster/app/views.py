e5ac1fc4e62441f2021993158a9325bc38bf9e28
Simon Löschke <simon-loeschke@posteo.de>
2022-12-03 15:32:12 +0100

    Indexeite initialisiert

--- burgermaster/app/views.py
+++ burgermaster/app/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1> Welcome </h1>')
+    return render(request,'index.html')
--- burgermaster/burgermaster/settings.py
+++ burgermaster/burgermaster/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'burgermaster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
